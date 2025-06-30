7c7326eec25c6294c17de560fbcaf23203e1b1e7
Josue M <156944263+21JosueMM@users.noreply.github.com>
2025-06-30 11:37:03 -0600

    Se agregaron los archivos script y css del archivo base y se implemento &acute

---
+++ js/testimonial-swiper.js
@@ -0,0 +1,1 @@
+var swiper=new Swiper('.testimonial-swiper',{loop:true,slidesPerView:3,spaceBetween:30,autoplay:{delay:4000,disableOnInteraction:false},breakpoints:{0:{slidesPerView:1},768:{slidesPerView:3}}});
